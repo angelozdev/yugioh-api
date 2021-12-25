64f170a6fe3972e545b426fb7cb841d873da7f31
Angelo Zambrano <angelozam17@gmail.com>
2021-12-24 22:10:40 -0500

    :recycle: refactor: improve magage icons

--- src/pages/Home/components/CardItem/index.tsx
+++ src/pages/Home/components/CardItem/index.tsx
@@ -42,76 +42,86 @@ const CardItem = memo(
   }: Props) => {
     const card = { name, description, images, id, type, archetype, imageIndex };
 
+    const onClick = () => {
+      onAddCard?.({
+        archetype,
+        atk: attack,
+        attribute,
+        card_images: images,
+        def: defense,
+        desc: description,
+        id,
+        name,
+        type,
+      });
+
+      onDeleteCard?.(String(id));
+    };
+
+    const icons = {
+      heart: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-red-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>
+      ),
+      trash: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-red-900"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+        </svg>
+      ),
+      success: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-green-600"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      ),
+    };
+
     return (
       <li className="border rounded-md shadow-md hover:shadow-lg relative overflow-hidden">
         <div className="bg-white rounded-full hover:shadow-md shadow-sm absolute top-3 right-2 p-1 leading-none hover:text-red-600 transition-colors">
           <button
             className="disabled:opacity-50"
             disabled={showSuccessIcon || isLoading}
-            onClick={() => {
-              onAddCard?.({
-                archetype,
-                atk: attack,
-                attribute,
-                card_images: images,
-                def: defense,
-                desc: description,
-                id,
-                name,
-                type,
-              });
-
-              onDeleteCard?.(String(id));
-            }}
+            onClick={onClick}
           >
-            {showSuccessIcon ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-green-600"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            ) : onAddCard ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-red-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-            ) : (
-              onDeleteCard && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-red-900"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                  />
-                </svg>
-              )
-            )}
+            {showSuccessIcon
+              ? icons.success
+              : onAddCard
+              ? icons.heart
+              : onDeleteCard && icons.trash}
           </button>
         </div>
         <Link className="w-full flex" state={{ card }} to={`/cards/${id}`}>
